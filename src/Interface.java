f5f478cbd837461b9c9ebe50d94164988ca68cb5
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-25 18:21:24 +0200

    Interface

---
+++ src/Interface.java
@@ -0,0 +1,20 @@
+interface test {
+
+    String color = "Blue";
+
+    void fillUp();
+}
+
+public class Interface implements test {
+    public static void main(String[] args) {
+        System.out.println(color);
+
+        Interface ex = new Interface();
+        ex.fillUp();
+    }
+
+    @Override
+    public void fillUp() {
+        System.out.println("Filled");
+    }
+}
